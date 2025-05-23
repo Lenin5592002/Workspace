3f2b8400e8947166e72c99f18185aea23b2753d2
lorena <lopezzelayalore@gmail.com>
2025-05-23 09:37:31 -0500

    Loren - Comentarios en Entity.java.

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -3,14 +3,13 @@ package entity;
 
 import java.awt.image.BufferedImage;
 
-
 public class Entity {
-    
-    public  int wordlX, wordlY;
-    public  int speed ;
-    public  BufferedImage up1,up2,up3,down1,down2,down3,right1,right2,right3,left1,left2,left3,normal ;
-    public String direction; 
-    public int spriteCounter = 0 ;
-    public int spriteNum = 1 ; 
-    
+
+    public int wordlX, wordlY; // jugador
+    public int speed;
+    public BufferedImage up1, up2, up3, down1, down2, down3, right1, right2, right3, left1, left2, left3, normal;
+    public String direction;
+    public int spriteCounter = 0; // para controlar cuando cambiar la imagen
+    public int spriteNum = 1;
+
 }
