14db91d4b17a414a457a1c7f5d60acade64f5ec2
Lenin5592002 <lenin559mce@gmail.com>
2025-05-30 16:57:15 -0500

    C5

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -2,17 +2,8 @@ package Main;
 
 public class Main {
     public static void main(String[] args) {
-        JFrame window = new JFrame();
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // cerrar la ventana correctamente
-        window.setResizable(false);// no redimenzionar a la pantalla
-        window.setTitle("Pokemon");
-        GamePanel gamepanle = new GamePanel();
-        window.add(gamepanle);// se añade al panel a la ventana
-        window.pack();
-        window.setLocationRelativeTo(null);// centrar
-        window.setVisible(true);
-        gamepanle.startGame();
-        
+        Menu menu = new Menu();
+        menu.mostrar();
         //HOLAAJSJAJSAJS 
         //soy victor
     }
